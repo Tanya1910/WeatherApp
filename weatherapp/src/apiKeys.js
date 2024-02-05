58752773143fcba60e66cebf26ad3935bb525eb3
Tanya1910 <writertanya3997@gmail.com>
2024-02-05 22:47:05 +0530

    Adding live weather feature to app

---
+++ weatherapp/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9f494647243769ac2801b38e7b712405",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
